6fe77d73084a8a104516f0048e42ff20b81645fc
AYESDIE <34449856+AYESDIE@users.noreply.github.com>
2018-06-27 17:59:04 +0530

    Update moon.py

--- moon.py
+++ moon.py
@@ -8,11 +8,6 @@ def fr_(url):
     faces = face_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30, 30))
     return (format(len(faces)))
 
-
-def main():
-    test_image = "http://www.gstatic.com/tv/thumb/persons/835149/835149_v9_bb.jpg"
-    print(json_(fr_(url=test_image)))
-
 def json_(status,face=0,url=""):
     if(status=="fail"):
         json = '{"status": "error","data": null,"message": "An Error has occurred"}'
@@ -20,5 +15,9 @@ def json_(status,face=0,url=""):
         json = '{"status": "success","data": {"url": "'+url+'","faces": "'+str(face)+'" },"message": null}'
     return json
 
+def main():
+    test_image = "http://www.gstatic.com/tv/thumb/persons/835149/835149_v9_bb.jpg"
+    print(json_(fr_(url=test_image)))
+    
 if (__name__ == '__main__'):
     main()
