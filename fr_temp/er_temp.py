5f63ca38d55e5c6c187583bb8c0e96ff7fe752fc
AYESDIE <34449856+AYESDIE@users.noreply.github.com>
2018-07-02 18:23:20 +0530

    Adding CNN

--- fr_temp/er_temp.py
+++ fr_temp/er_temp.py
@@ -25,3 +25,20 @@ emotions = pd.get_dummies(dataset['emotion']).as_matrix()
 
 #   Spliting into test set and training set
 X_train, X_test, y_train, y_test = train_test_split(faces, emotions, test_size=0.2, random_state=0)
+
+
+# CNN Model 
+
+
+classifier = Sequential()
+classifier.add(Convolution2D(48,3,3, input_shape=(width,height,1),activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+classifier.add(Flatten())
+classifier.add(Dense(output_dim = 96,activation='relu'))
+classifier.add(Dense(output_dim = 96,activation='relu'))
+classifier.add(Dense(output_dim = 7,activation='softmax'))
+classifier.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+checkpoint = ModelCheckpoint('data/cnn.h5', monitor='val_loss', verbose=1, save_best_only=True)
+classifier.fit(np.array(X_train),np.array(y_train),batch_size=100,epochs=25)
+
+
